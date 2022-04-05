aa3f49e12998f4a0ac9c43f7075322b32665b374
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-05 10:51:18 -0400

    commit tree 91ee6b2052a56630b0c3d58f730978c6cba6a3da

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+   return `This Saturday, I want to ${activity}!`
+}
+                        // this is the 'default arguement'
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+                        //this is the 'visual flair'
+function wrapAdjective(style = '*') {
+    return function innerFunction(adjective = 'special') {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
